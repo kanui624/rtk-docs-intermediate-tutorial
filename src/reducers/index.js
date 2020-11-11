7d4fa21ad7b5bb786f73efc29ea2202304e5e048
Kanui <marley.kanui@gmail.com>
2020-11-11 11:27:13 -0600

    rtk-intermediate-tutorial - global commit 07 - update combineReducer function with new todosSlice

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
-import { combineReducers } from 'redux'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter'
+import { combineReducers } from 'redux';
+// import todos from './todos'
+import todosReducer from 'features/todos/todosSlice';
+import visibilityFilter from './visibilityFilter';
 
 export default combineReducers({
-  todos,
-  visibilityFilter
-})
+  //  todos,
+  todos: todosReducer,
+  visibilityFilter,
+});
