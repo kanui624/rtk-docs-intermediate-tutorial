17301e46f164c8807b4e286ba42ad855f408a4e3
Kanui <marley.kanui@gmail.com>
2020-11-11 12:11:15 -0600

    rtk-intermediate-tutorial - global commit 12 - updated visibleTodoList with shorthand syntax

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -4,22 +4,42 @@ import { toggleTodo } from '../features/todos/todosSlice';
 import TodoList from '../components/TodoList';
 // import { VisibilityFilters } from '../actions';
 import { VisibilityFilters } from '../features/filters/filtersSlice';
+import { createSelector } from '@reduxjs/toolkit';
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
-      return todos;
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter((t) => t.completed);
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter((t) => !t.completed);
-    default:
-      throw new Error('Unknown filter: ' + filter);
+// const getVisibleTodos = (todos, filter) => {
+//   switch (filter) {
+//     case VisibilityFilters.SHOW_ALL:
+//       return todos;
+//     case VisibilityFilters.SHOW_COMPLETED:
+//       return todos.filter((t) => t.completed);
+//     case VisibilityFilters.SHOW_ACTIVE:
+//       return todos.filter((t) => !t.completed);
+//     default:
+//       throw new Error('Unknown filter: ' + filter);
+//   }
+// };
+
+const selectTodos = (state) => state.todos;
+const selectFilter = (state) => state.visibilityFilter;
+const selectVisibleTodos = createSelector(
+  [selectTodos, selectFilter],
+  (todos, filter) => {
+    switch (filter) {
+      case VisibilityFilters.SHOW_ALL:
+        return todos;
+      case VisibilityFilters.SHOW_COMPLETED:
+        return todos.filter((t) => t.completed);
+      case VisibilityFilters.SHOW_ACTIVE:
+        return todos.filter((t) => !t.completed);
+      default:
+        throw new Error('Unknown filter: ' + filter);
+    }
   }
-};
+);
 
 const mapStateToProps = (state) => ({
-  todos: getVisibleTodos(state.todos, state.visibilityFilter),
+  // todos: getVisibleTodos(state.todos, state.visibilityFilter),
+  todos: selectVisibleTodos(state),
 });
 
 // const mapDispatchToProps = (dispatch) => ({
